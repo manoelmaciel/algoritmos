accc88b42b6bfbfe91508d4f50fbd18d3fd98920
manoelmaciel <manoelbraz.maciel@gmail.com>
2025-05-02 18:11:51 -0300

    Capítulo 1
    Ainda não consegui concluir

--- tests/buscaBinaria/javaScript/script.js
+++ tests/buscaBinaria/javaScript/script.js
@@ -1,21 +1,46 @@
 /* sex 02 mai 2025 11:42:03 
    
-   script.js
+   *** script.js
    
-   // criando a lista 
+  */
    
-   
-   function criarLista(size) {
-     while(x  <= size){
-      lista[x-1] = x;
-     }
-     return lista;
+  // função para criar uma lista ordenada
+  function criarLista(size) {
+   let lista = [];
+   let counter = 1;
+   let index = 0;     
+   while(counter <= size){
+     lista[index] = counter;
+     index = counter;
+     counter++;
    }
-   
-   myList[] = criarLista(100); 
-   
-   console.log(map(myList));
-   
-   // ---------------------------------------
-   
-   
+   return lista;
+  }
+
+  // criando uma lista 
+  myList = criarLista(1000); 
+
+  // mostrando a lista
+  // console.log(myList);
+
+  // ---------------------------------------
+
+  // busca binária
+  buscaBinaria = (lista, item) => {
+    let baixo = 0;
+    let alto = lista.length - 1;
+    while (baixo <= alto) {
+      meio = Math.trunc((baixo + alto) / 2);
+      chute = lista[meio];
+      if (chute == item) 
+        return meio;
+      if (chute > item) 
+        alto = meio - 1;
+      else 
+        baixo = meio + 1;
+    } 
+    return null;
+  }
+
+  // busca
+  console.log(buscaBinaria(myList, 551));
